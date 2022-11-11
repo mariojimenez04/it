6d0ff661c8a45394373ef52b8c1849a09026a1fc
mariojimenez04 <cuentas.alexjimenez@gmail.com>
2022-11-10 22:01:57 -0600

    Version 0.0.15

---
+++ app/Http/Controllers/MovimientoProductoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MovimientoProducto;
+use Illuminate\Http\Request;
+
+class MovimientoProductoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\MovimientoProducto  $movimientoProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MovimientoProducto $movimientoProducto)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\MovimientoProducto  $movimientoProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MovimientoProducto $movimientoProducto)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\MovimientoProducto  $movimientoProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MovimientoProducto $movimientoProducto)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\MovimientoProducto  $movimientoProducto
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MovimientoProducto $movimientoProducto)
+    {
+        //
+    }
+}
---
+++ database/migrations/2022_11_10_195724_create_movimiento_productos_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movimiento_productos', function (Blueprint $table) {
+            $table->id();
+            $table->string('movimiento')->nullable();
+            $table->string('usuario')->nullable();
+            $table->string('equipo')->nullable();
+            $table->string('direccion_ip')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movimiento_productos');
+    }
+};
